a3bec408adf2643fb3aa5fd047757a9891e6aa06
vchkoz <vchkoz@mail.ru>
2022-02-08 21:52:57 +0300

    Поправил маленько

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, request, render_template, flash, session, url_for, redirect, abort
 from datetime import datetime
 
-
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///blog.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
@@ -11,65 +10,49 @@ app.config["SECRET_KEY"] = "hs6dkjh1a3fou2hebfhb"
 db = SQLAlchemy(app)
 
 
-class Users(db.Model):
+class Passwords(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    psw = db.Column(db.String(500), nullable=True)
+    psw = db.Column(db.String(500), nullable=False)
 
     def __repr__(self):
-        return f"<Users {self.id}>"
-
-# class Data(db.Model): С этим что-то не так
-#     id = db.Column(db.Integer, primary_key=True)
-#     data = db.Column(db.DateTime, default=datetime.utcnow)
-#     user_id = db.Column(db.Integer, db.ForeignKey("Users.id"))
-#
-#     def __repr__(self):
-#         return f"<data {self.id}>"
+        return f"{self.psw}"
 
 
-@app.route("/main", methods=["POST", "GET"])
+class Data(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"<data {self.id}>"
+
+
+@app.route("/", methods=["GET"])  # Главная
 def main():
+    users = Passwords.query.all()
+    print(users)
+    return render_template("main.html", title="Введите пароль")
+
+
+@app.route("/add-password", methods=["POST", "GET"])  # страница добавления нового пароля
+def add_password():
     if request.method == "POST":
-        if len(request.form["email"]) > 2:
-            flash("Отправлено")
-        else:
-            flash("Ошибка")
-    return render_template("contact.html", title="Введите пароль")
+        password = request.form['psw']
+        arr = Passwords(psw=password)
+        # надо будет сделать проверку на то что парольне пустой и на то что такого пароля ешё нет в бд
+        try:
+            db.session.add(arr)
+            db.session.commit()
+        except:
+            pass
+        return redirect('/')
 
-
-@app.route("/profile/<username>")
-def profile(email):
-    if 'userLogged' not in session or session['userLogged'] != email:
-        abort(401)
-    return f"Пользователь"
-
-
-@app.route("/login", methods=["POST", "GET"])
-def login():
-    if 'userLogged' in session:
-        return redirect(url_for('profile', email=session['userLogged']))
-    elif request.form['psw'] == "123":
-        session['userLogged'] = request.form['email']
-        return redirect(url_for('profile', email=session['userLogged']))
-
-    return render_template('login.html', title="Добавление пользователя")
-
-
-@app.route('/d', methods=["GET"])
-def create_article():
-
-    arr = Users(psw=12345)
-    try:
-        db.session.add(arr)
-        db.session.commit()
-    except:
-        pass
-    return 'добавлено'
+    paswords = Passwords.query.all()
+    return render_template('pas-add.html', paswords=paswords)
 
 
 @app.errorhandler(404)
 def pageNoFound(error):
-    return render_template("page404.html", title="Страница не найдена"), 404
+    return 'страница не найдена'
 
 
 if __name__ == "__main__":
